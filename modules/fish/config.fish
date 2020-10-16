d9a81e9ba5a861bf305d76862fd51042ec7e7df6
matsumoto_t <ht_rk_km_-effort-2@i.softbank.jp>
2020-10-16 14:09:06 +0900

    tidy

---
+++ modules/fish/config.fish
@@ -0,0 +1,15 @@
+# load functions.fish
+source ~/.config/fish/functions.fish
+
+# initialize
+function fish_prompt
+    if [ $status -eq 0 ]
+        set status_face (set_color green)"(*´ω｀*) < "
+    else
+        set status_face (set_color red)"｡+ﾟ(∩´﹏'∩)ﾟ+｡ < "
+    end
+    printf '%s %s' (set_color yellow)(prompt_pwd) $status_face
+end
+
+# alias
+alias cdd 'cd ~/Desktop'
